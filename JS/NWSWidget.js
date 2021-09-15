bfd5ffd4751202247445f994e28731f8d2979fcc
Larry G.R <lgaitanrodriguez@outlook.com>
2021-09-15 01:44:13 -0400

    updated content, added the about page, and fixed the naming of some the pages

--- JS/NWSWidget.js
+++ JS/NWSWidget.js
@@ -2,13 +2,11 @@
 
 //Bringing in the data from National Weather Service.
 //More specifically the data for dahlonega!
-console.log("Loaded");
 fetch('https://api.weather.gov/gridpoints/FFC/62,122/forecast') 
     .then((res)=>{
         return res.json();
     })
     .then(weather=> {
-        console.log(weather);
         //Time to populate the cards in the widget?
         for(i = 0; i < 7;i++){
             //This is starting to remind me of a high school JS esque project I did..
@@ -18,6 +16,6 @@ fetch('https://api.weather.gov/gridpoints/FFC/62,122/forecast')
             $("#"+i+"-icon").attr("src",weather.properties.periods[i].icon);
             $("#"+i+"-temp").html(weather.properties.periods[i].temperature+"F");
             $("#"+i+"-detail").html(weather.properties.periods[i].shortForecast);
+            //Im honestly suprised this all worked.
         }
-        console.log(weather.properties.periods[0].name);
     }); 
